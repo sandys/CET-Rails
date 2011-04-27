6402bf138ded0efd39b2b9bcc7e86d2074ec656e
Sandeep Srinivasa <sandys@gmail.com>
2011-04-27 11:44:45 +0530

    empty structure created

---
+++ CET/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CET::Application.config.secret_token = '1276187ff280689ff5b7ab9df14f1f71aa5b273ed698410d411060cf7a56c7e616fbcf63f4b033ee1c25ee3e798dfe13c66fb2e5820080df86390e4fec92f0a2'
---
+++ README.markdown
@@ -0,0 +1,1 @@
+rails new CET -d mysql -m http://jruby.org/rails3.rb
