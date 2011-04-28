9cf76be79b56c1ec17c50b11a29b77bcd6eba959
Akshay Gupta <akshaygupta.dev@gmail.com>
2011-04-28 14:29:27 +0530

    creation of zip file

--- CET/lib/generate_contract.rb
+++ CET/lib/generate_contract.rb
@@ -1,6 +1,6 @@
 require 'prawn'
 require "prawn/measurement_extensions"
-
+require 'zip/zipfilesystem'
 
 module GenerateContract
 
@@ -14,6 +14,7 @@ module GenerateContract
 		end
 	end
 	
+	
 	class XLS
 		def self.create(contract)
 			book = Spreadsheet::Workbook.new
@@ -23,4 +24,33 @@ module GenerateContract
 			book.write(Rails.root.join('files/xls',"#{contract.id}_#{contract.name}.xls"))
 		end
 	end
+	
+	
+	class ZIP
+		attr_accessor :list_of_file_paths, :zip_file_path
+
+		def initialize(contract)
+			filename = "#{contract.id}_#{contract.name}"
+      @zip_file_path = Rails.root.join('files/compress', "#{filename}.zip")
+      @list_of_file_paths = [Rails.root.join('files/pdf',"#{filename}.pdf"), Rails.root.join('files/xls', "#{filename}.xls")]
+		end
+
+		def create
+		  zip = Zip::ZipFile.open(self.zip_file_path, Zip::ZipFile::CREATE) do | zipfile |
+		    @list_of_file_paths.each do | file_path |
+		      if File.exists?file_path
+		        file_name = File.basename( file_path )
+		        if zipfile.find_entry( file_name )
+		        	zipfile.replace( file_name, file_path )
+		        else
+		        	zipfile.add( file_name, file_path)
+		        end
+		      else
+		      	puts "Warning: file #{file_path} does not exist"
+		      end
+		    end
+		  end
+		end
+	end
+		
 end
