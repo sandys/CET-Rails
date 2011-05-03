57a5a2fbbfbc953720d77d64b0ad910b6c96f5c4
Akshay Gupta <akshaygupta.dev@gmail.com>
2011-05-03 17:18:55 +0530

    skipped file

--- CET/lib/hmis.rb
+++ CET/lib/hmis.rb
@@ -23,6 +23,14 @@ module HMIS
     def get_sales_counselor(sales_type_id = nil)
       {"Robert Lacey" => "1-Robert-Lacey", "John Brooks" => "2-John-Brooks", "Paul Shipman" => "3-Paul-Shipman", "Dennis Bonner" => "4-Dennis-Bonner", "House Account" => "5-House-Account", "Ben Upton" => "6-Ben-Upton", "John Carnes" => "7-John-Carnes", "Donald Garrison" => "8-Donald-Garrison", "Dale Granger" => "9-Dale-Granger", "Craig Wolf" => "10-Craig-Wolf"}
     end
+    
+    def name_types
+      [ "Purchaser", "Co-purchaser", "Beneficiary", "Deceased"]
+    end
+    
+    def discount_reason
+      ["ICFA", "Manager Approved", "Insurance Allowance", "Pre-developed", "American Hero", "Emergency Burial Cert", "Senior Care Seminar", "Jewish Funeral Directors Assoc", "Trust Allowance", "Public Assistance", "Family Heritage", "Clergy", "Package Discount", "Competitor Match", "Davie", "Hospice Discount", "Preneed Tax Prepaid", "Promotional", "Infant Service", "Professional Courtesy", "Veteran Discount", "Charitable Discount", "Math Error", "Presentation", "Employee Discount", "Holiday Incentive", "Community Outreach"]
+    end
 	
   end
 end
