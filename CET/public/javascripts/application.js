132ceab78a3521af4fed6a63046924124b7ee9ef
Akshay Gupta <akshaygupta.dev@gmail.com>
2011-05-04 16:15:02 +0530

    all field attributes under contract type

--- CET/public/javascripts/application.js
+++ CET/public/javascripts/application.js
@@ -3,16 +3,16 @@
 
 $(function() {
 
-  $("#sales_batch_date").datepicker();
-  $("#sales_txn_date").datepicker();
-  $("#sales_date").datepicker();
+  $("#contract_sales_batch_date").datepicker();
+  $("#contract_sales_txn_date").datepicker();
+  $("#contract_sales_date").datepicker();
   $(".down_payment_date").datepicker();
-  $("#interest_payment_start_date").datepicker();
+  $("#contract_interest_payment_start_date").datepicker();
     
   $("#contract_location_id").change(function() {
     var id = $("#contract_location_id").val();
     $.post('sales_type', {location_id : id }, function(data) {
-      $("#sales_type").html(data);
+      $("#contract_sales_type").html(data);
     });
     
     $.post('item_group_code', {location_id : id }, function(data) {
@@ -21,17 +21,17 @@ $(function() {
 
   });
   
-  $("#sales_type").change(function() {
-    var id = $("#sales_type").val();
+  $("#contract_sales_type").change(function() {
+    var id = $("#contract_sales_type").val();
     $.post('sales_txn_type', {sales_type_id : id }, function(data) {
-      $("#sales_txn_type").html(data);
+      $("#contract_sales_txn_type").html(data);
     });
     
     $.post('sales_counselor', {sales_type_id : id }, function(data) {
-      $("#sales_primary_counselor").html(data);
-      $("#sales_secondary_counselor_1").html(data);
-      $("#sales_secondary_counselor_2").html(data);
-      $("#sales_secondary_counselor_3").html(data);
+      $("#contract_sales_primary_counselor").html(data);
+      $("#contract_sales_secondary_counselor_1").html(data);
+      $("#contract_sales_secondary_counselor_2").html(data);
+      $("#contract_sales_secondary_counselor_3").html(data);
     });
     
     $.post('payment_type', {sales_type_id : id }, function(data) {
@@ -39,19 +39,19 @@ $(function() {
     });
     
      $.post('interest_term', {sales_type_id : id }, function(data) {
-      $("#interest_term").html(data);
+      $("#contract_interest_term").html(data);
     });
     
      $.post('interest_method', {sales_type_id : id }, function(data) {
-      $("#interest_method").html(data);
+      $("#contract_interest_method").html(data);
     });
     
   });
   
-  $("#sales_txn_type").change(function() {
-    var id = $("#sales_txn_type").val();
+  $("#contract_sales_txn_type").change(function() {
+    var id = $("#contract_sales_txn_type").val();
     $.post('sales_lead_source', {sales_txn_type_id : id }, function(data) {
-      $("#sales_lead_source").html(data);
+      $("#contract_sales_lead_source").html(data);
     });
   });
   
