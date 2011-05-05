c246f35cf31b301cab36e438e428b3ab71576f47
Akshay Gupta <akshaygupta.dev@gmail.com>
2011-05-05 12:31:41 +0530

    pdf file generation

--- CET/lib/generate_contract.rb
+++ CET/lib/generate_contract.rb
@@ -1,4 +1,5 @@
 require 'prawn'
+require 'prawn/layout'
 require "prawn/measurement_extensions"
 require 'zip/zipfilesystem'
 
@@ -7,60 +8,103 @@ module GenerateContract
 	class PDF
 		def self.create(contract, user)
 			pdf = Prawn::Document.new(:page_layout => :portrait, :left_margin => 1.cm, :right_margin => 1.cm, :top_margin => 1.cm,  :bottom_margin => 1.cm, :page_size => 'A4')
-			pdf.text 'Location: #{contract["location_id"]}, Batch_Date: #{contract["sales_batch_date"]}, Txn_Date: #{contract["sales_txn_date"]}'
-			pdf.text 'Contract Number: ##{contract["contract_no"]}'
+			pdf.text "Location: #{contract[:location_id.to_s]}"
 			pdf.move_down(5)
-			pdf.text 'Sales Type: #{contract["sales_type"]}, User Name: #{user.email}, Password: '
+			pdf.text "Contract Number: ##{contract[:contract_no.to_s]}"
 			pdf.move_down(5)
-			pdf.text 'Txn_Type: #{contract["sales_txn_type"]}, HMIS URL: #{contract["hmis_url"]}'
+			pdf.text "Batch_Date: #{contract[:sales_batch_date.to_s]}"
 			pdf.move_down(5)
-			pdf.text 'Sales Date: #{contract["sales_date"]}, Terms: #{contract["interest_term"]}, Payment Start Date: #{contract["interest_payment_start_date"]}, Interest Method: #{contract["interest_method"]}'
+			pdf.text "Txn_Date: #{contract[:sales_txn_date.to_s]}"
 			pdf.move_down(5)
-			pdf.text 'Sales Need: #{contract["sales_need"]}, Interest Rate: #{contract["interest_rate"]}, Days Interest Free: #{contract["interest_free_days"]}, Forgive Interest: #{contract["interest_forgive"]}'
+			pdf.text "Sales Type: #{contract[:sales_type.to_s]}"
 			pdf.move_down(5)
-			pdf.text 'Lead Source: #{contract["sales_lead_source"]}'
+			pdf.text "User Name: #{user.email}, Password: "
 			pdf.move_down(5)
-			pdf.text 'Primary Arranger: #{contract["sales_primary_counselor"]}, Secondary Arranger1: #{contract["sales_secondary_counselor_1"]}, Secondary Arranger2: #{contract["sales_secondary_counselor_2"]}, Secondary Arranger3: #{contract["sales_secondary_counselor_3"]}'
+			pdf.text "Txn_Type: #{contract[:sales_txn_type.to_s]}"
 			pdf.move_down(5)
-			pdf.text "Name Type, First Name, Middle Name, Last Name, Address, City, State, Zip, Phone, DOB, DOD, SS No., Name Id"
-			contract["personal"].each do |key,value| 
+			pdf.text "HMIS URL: #{contract[:hmis_url.to_s]}"
+			pdf.move_down(5)
+			pdf.text "Sales Date: #{contract[:sales_date.to_s]}"
+			pdf.move_down(20)
+			pdf.text "Terms: #{contract[:interest_term.to_s]}, Payment Start Date: #{contract[:interest_payment_start_date.to_s]}, Interest Method: #{contract[:interest_method.to_s]}"
+			pdf.move_down(5)
+			pdf.text "Sales Need: #{contract[:sales_need.to_s]}, Interest Rate: #{contract[:interest_rate.to_s]}, Days Interest Free: #{contract[:interest_free_days.to_s]}, Forgive Interest: #{contract[:interest_forgive.to_s]}"
+			pdf.move_down(5)
+			pdf.text "Lead Source: #{contract[:sales_lead_source.to_s]}"
+			pdf.move_down(5)
+			pdf.text "Primary Arranger: #{contract[:sales_primary_counselor.to_s]}, Secondary Arranger1: #{contract[:sales_secondary_counselor_1.to_s]}, Secondary Arranger2: #{contract[:sales_secondary_counselor_2.to_s]}, Secondary Arranger3: #{contract[:sales_secondary_counselor_3.to_s]}"
+			pdf.move_down(20)
+			
+			personal_details = contract["personal"].map do |key,value| 
 				unless (value['first_name'].blank? and value['last_name'].blank?)
-					pdf.text '#{value["name_type"]}, #{value["first_name"]}, #{value["middle_name"]}, #{value["last_name"]}, #{value["address"]}, #{value["city"]}, #{value["state"]}, #{value["zipcode"]}, #{value["phone"]}, #{value["dob"]}, #{value["dod"]}, #{value["ssn"]}'
-					pdf.move_down(5)
+					[ value[:name_type.to_s], 
+						value[:first_name.to_s],
+						value[:middle_name.to_s], 
+						value[:last_name.to_s], 
+						value[:address.to_s], 
+						value[:city.to_s], 
+						value[:state.to_s], 
+						value[:zipcode.to_s], 
+						value[:phone.to_s], 
+						value[:dob.to_s], 
+						value[:dod.to_s], 
+						value[:ssn.to_s]
+					]
 				else
-					pdf.text ""
+					["", "", "", "", "", "", "", "", "", "", "", ""]
 				end
 			end
+			pdf.table personal_details, :row_colors => ["FFFFFF", "DDDDDD"] 
+			#, :headers => ["Name Type", "First Name", "Middle Name", "Last Name", "Address", "City", "State", "Zip", "Phone", "DOB", "DOD", "SS No.", "Name Id"] 
 			
-			pdf.text "Item Cd, Item Descr, Quantity, Price, Discount, Discount Reason, Sales Need"
-			contract["item"].each do |key,value| 
+			pdf.move_down(20)
+			personal_items= contract["item"].map do |key,value| 
 				unless value['final_code'].blank? 
-					pdf.text '#{value["final_code"]}, #{value["final_desc"]}, #{value["quantity"]}, #{value["price"]}, #{value["discount"]}, #{value["discount_reason"]}, #{value["sales_need"]}'
-					pdf.move_down(5)
+					[ value[:final_code.to_s],  
+						value[:final_desc.to_s], 
+						value[:quantity.to_s], 
+						value[:price.to_s], 
+						value[:discount.to_s], 
+						value[:discount_reason.to_s], 
+						value[:sales_need.to_s]
+					]
 				else
-					pdf.text ""
+					["", "", "", "", "", "", ""]
 				end
 			end
 			
-			pdf.text "Down Pymt Dt, Amount, Down Pymt_Type, Remarks"
-			contract["payment"].each do |key,value| 
+			pdf.table personal_items, :row_colors => ["FFFFFF", "DDDDDD"] 
+			#, :headers => ["Item Cd", "Item Descr", "Quantity", "Price", "Discount", "Discount Reason", "Sales Need"]
+
+			pdf.move_down(20)						
+			payments = contract["payment"].map do |key,value| 
 				unless value['amount'].blank? 
-					pdf.text '#{value["date"]}   #{value["amount"]}   #{value["type"]}  #{value["remark"]}'
-					pdf.move_down(5)
+					[ value[:date.to_s],
+						value[:amount.to_s],
+						value[:type.to_s], 
+						value[:remark.to_s]
+					]
 				else
-					pdf.text ""
+					[ "", "","", ""]
 				end
 			end
+			pdf.table payments, :row_colors => ["FFFFFF", "DDDDDD"] 
+			#, :headers => ["Down Pymt Dt", "Amount", "Down Pymt_Type", "Remarks"]
 			
-			pdf.text "Image Name, Document Type, Uploaded File"
-			contract["file"].each do |key,value| 
+			pdf.move_down(20)
+			images  = contract["file"].map do |key,value| 
 				unless value['image_name'].blank? 
-					pdf.text '#{value["image_name"]}  #{value["upload_type"]}'
-					pdf.move_down(5)
+					[ value[:image_name.to_s],
+					  value[:upload_type.to_s]
+					]
 				else
-					pdf.text "" 
+					["", ""]
 				end
 			end
+
+			pdf.table images,:row_colors => ["FFFFFF", "DDDDDD"]
+			#, :headers =>  ["Image Name", "Document Type",   "Uploaded File"]
+			
 			
 			pdf.render_file(Rails.root.join('files/pdf',"#{contract['location_id']}_#{contract['contract_no']}.pdf"))
 		end
