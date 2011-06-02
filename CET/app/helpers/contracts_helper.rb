8c3ee3f4fc5bcbdb3abc6431bae6910de73a0e7a
Akshay Gupta <akshaygupta.dev@gmail.com>
2011-06-02 15:55:03 +0530

    total amount calculation

--- CET/app/helpers/contracts_helper.rb
+++ CET/app/helpers/contracts_helper.rb
@@ -3,4 +3,15 @@ module ContractsHelper
 		options.merge!({:include_blank => true})
 		select_tag(id, options_for_select(collection), options )
 	end
+	
+	def total_cost(contract)
+		items = contract.data["item"]
+		total_cost = 0
+		(1..items.length).each do |i|
+			price = items[i.to_s]["price"].to_f * items[i.to_s]["quantity"].to_i
+			discount = price * items[i.to_s]["discount_percent"].to_f / 100
+			total_cost += (price - discount)
+		end
+		return total_cost
+	end
 end
