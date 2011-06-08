0ea70edba9b883dbd1fede377ece9e8af613009f
Sandeep Srinivasa <sandys@gmail.com>
2011-06-08 12:40:04 +0530

    added new environment and optimized the production environment to have lesser logging

--- CET/config/environments/sss_development.rb
+++ CET/config/environments/sss_development.rb
@@ -19,7 +19,6 @@ CET::Application.configure do
   # just comment this out and Rails will serve the files
 
   # See everything in the log (default is :info)
-  # config.log_level = :debug
    config.log_level = :error
 
   # Use a different logger for distributed setups
